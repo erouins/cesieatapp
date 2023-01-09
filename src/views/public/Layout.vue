fa2dc81367e74719f73f954c2ca9f49693d563a4
Josh <jdelriu@entelekia.fr>
2023-01-09 15:18:30 +0100

    création des routes

--- src/views/public/Layout.vue
+++ src/views/public/Layout.vue
@@ -1,20 +1,49 @@
 <template>
-    <div class = 'public'>
-        <PublicNav/>
-    </div>
-    <router-view/>
+    <body>
+        <div class='header'>
+            <p>test header</p>
+            
+        </div>
+        <div class='main-content'>
+            <p>test layout</p>
+        </div>
+        <router-view />
+    </body>
+    <footer>
+
+    </footer>
 </template>
 
 <script>
 import PublicNav from '@/components/PublicNav.vue'
 export default {
     name: "PublicLayout",
-    components:{
+    components: {
         PublicNav
     }
 }
 </script>
 
 <style>
+    body {
+        margin: 0px;
+        background-color: #eee;
+        color: #212529;
+        font-family: 'Roboto', 'Open Sans', Arial, Helvetica, sans-serif;
+        text-align: center;
+        font-size: 1em;
+    }
 
+    .header {
+        width: 100%;
+        height: 3.5em;
+        position: fixed;
+        top: 0;
+        background-color: red;
+    }
+
+    .main-content {
+        margin-top: 4.5em;
+        width: 100%;
+    }
 </style>
