d8d53dabba5466668ec2287f62653e1f8badc560
erouins <p.hugo91@orange.fr>
2023-01-11 11:50:45 +0100

    modif registration system

--- src/views/clients/Register.vue
+++ src/views/clients/Register.vue
@@ -4,15 +4,15 @@
             <div class="card-title">Add your client informations</div>
             <form @submit.prevent="register">
                 <div class="input-label"><label for="client_firstname">First name</label></div>
-                <input class="input-field-auth" type="text" id="client_firstname" v-model="client.firstName"
+                <input class="input-field-auth" type="text" id="client_firstname" v-model="profil.firstName"
                     title="The first name must contains only letters" pattern="^[A-Za-zÀ-ÿ][A-Za-zÀ-ÿ\s]*[A-Za-zÀ-ÿ]$"
                     required autocomplete />
                 <div class="input-label"><label for="client_lastname">Last name</label></div>
-                <input class="input-field-auth" type="text" id="client_lastname" v-model="client.lastName"
+                <input class="input-field-auth" type="text" id="client_lastname" v-model="profil.lastName"
                     title="The first name must contains only letters" pattern="^[A-Za-zÀ-ÿ][A-Za-zÀ-ÿ\s]*[A-Za-zÀ-ÿ]$"
                     required autocomplete />
                 <div class="input-label"><label for="client_address">address</label></div>
-                <input class="input-field-auth" type="text" id="client_address" v-model="client.address" required/>
+                <input class="input-field-auth" type="text" id="client_address" v-model="profil.address" required/>
                 <button class="submit-button" type="submit">submit</button>
             </form>
         </div>
@@ -24,8 +24,7 @@ export default {
     name: "ClientRegister",
     data() {
         return {
-            client: {
-                id: localStorage.getItem('userId'),
+            profil: {
                 firstName: '',
                 lastName: '',
                 address: '',
@@ -36,13 +35,14 @@ export default {
 
     methods: {
         register() {
-            fetch('http://localhost:3001/delivery/create', {
+            fetch('http://localhost:3001/client/create', {
                 headers: {
                     'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'Authorization': "Bearer " + localStorage.getItem('token'),
                 },
                 method: 'POST',
-                body: JSON.stringify(this.client)
+                body: JSON.stringify({'profil': this.profil, 'userId': localStorage.getItem('userId')}, )
             })
             .then((blob) => blob.json())
             .then((data) => {
--- src/views/restaurants/Register.vue
+++ src/views/restaurants/Register.vue
@@ -4,17 +4,19 @@
             <div class="card-title">Add your restaurant informations</div>
             <form @submit.prevent="register">
                 <div class="input-label"><label for="restaurant_name">name</label></div>
-                <input class="input-field-auth" type="text" id="restaurant_name" v-model="restaurant.lastName"
+                <input class="input-field-auth" type="text" id="restaurant_name" v-model="profil.name"
                     title="The first name must contains only letters" pattern="^[A-Za-zÀ-ÿ][A-Za-zÀ-ÿ\s]*[A-Za-zÀ-ÿ]$"
                     required autocomplete />
                 <div class="input-label"><label for="restaurant_address">address</label></div>
-                <input class="input-field-auth" type="text" id="restaurant_address" v-model="restaurant.address" required/>
+                <input class="input-field-auth" type="text" id="restaurant_address" v-model="profil.address" required/>
                 <div class="input-label"><label for="restaurant_city">city</label></div>
-                <input class="input-field-auth" type="text" id="restaurant_city" v-model="restaurant.city" required/>
+                <input class="input-field-auth" type="text" id="restaurant_city" v-model="profil.city" required/>
+                <div class="input-label"><label for="restaurant_zipcode">zipCode</label></div>
+                <input class="input-field-auth" type="text" id="restaurant_zipcode" v-model="profil.zipCode" required/>
                 <div class="input-label"><label for="restaurant_description">add a description</label></div>
-                <input class="input-field-auth" type="text" id="restaurant_description" v-model="restaurant.description"/>
+                <input class="input-field-auth" type="text" id="restaurant_description" v-model="profil.description"/>
                 <div class="input-label"><label for="restaurant_image">image</label></div>
-                <input class="input-field-auth" type="text" id="restaurant_image" v-model="restaurant.image"/>
+                <input class="input-field-auth" type="text" id="restaurant_image" v-model="profil.image"/>
                 
                 <button class="submit-button" type="submit">submit</button>
             </form>
@@ -27,11 +29,11 @@ export default {
     name: "RestaurantRegister",
     data() {
         return {
-            restaurant: {
-                id: localStorage.getItem('userId'),
+            profil: {
                 name: '',
                 address: '',
                 city: '',
+                zipCode: '',
                 description :'',
                 image: ''
             }
@@ -40,17 +42,19 @@ export default {
 
     methods: {
         register() {
-            fetch('http://localhost:3001/delivery/create', {
+            fetch('http://localhost:3001/restaurant/create', {
                 headers: {
                     'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'Authorization': "Bearer " + localStorage.getItem('token'),
+                    
                 },
                 method: 'POST',
-                body: JSON.stringify(this.restaurant)
+                body: JSON.stringify({'profil': this.profil, 'userId': localStorage.getItem('userId')}, )
             })
             .then((blob) => blob.json())
             .then((data) => {
-                
+                console.log('mhhm',data);
             }).catch((err) => {
                 console.log(err);
             });
