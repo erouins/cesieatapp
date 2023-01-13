2125524d1ea925d8b044c95b28822c46049f63b1
Josh <jdelriu@entelekia.fr>
2023-01-13 09:38:14 +0100

    Suppresion de la bar des tags

--- src/views/clients/components/Home.vue
+++ src/views/clients/components/Home.vue
@@ -1,13 +1,4 @@
 <template>
-  <div class="tag-bar">
-    <button class="tag-box">
-      test
-    </button>
-    <button class="tag-box">
-      fast-food
-    </button>
-  </div>
-  <hr>
   <div class="box-contener">
     <div class="restaurant-box">
       <div class="restaurant-box-image"></div>
@@ -38,39 +29,11 @@
 
 <script>
 export default {
-  name: "ClientHome"
-
+  name: "ClientHome",
 };
 </script>
 
 <style>
-  .tag-bar {
-    width: 100%;
-    height: 50px;
-    align-items: center;
-    display: flex;
-    flex-direction: row;
-    justify-content: left;
-  }
-
-  .tag-box {
-    cursor: pointer;
-    padding: 12px;
-    margin-right: 8px;
-    border: none;
-    border-radius: 10px;
-    font-size: 1em;
-    color: #2c3e50;
-    background: #fefefe;
-    box-shadow: 5px 0px 40px rgb(0 0 0 / 20%);
-    transition: 0.5s;
-  }
-
-  .tag-box:focus {
-    outline: none;
-    background: #93b721;
-  }
-
   .box-contener {
     height: 100%;
     width: 100%;
