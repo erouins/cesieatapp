56fc876004b1a5014498c1f71b61d0b4f460c518
erouins <p.hugo91@orange.fr>
2023-01-18 19:09:41 +0100

    delete menu/article from restaurants

--- src/views/restaurants/components/ArticleCard.vue
+++ src/views/restaurants/components/ArticleCard.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="menu_article_card">
     <ArticleCard v-bind:article="this.item" />
-    <button type='button' @click="goToUpdatePage">
+    <button type="button" @click="goToUpdatePage">
       <img src="@/assets/modify.png" alt="" />
+    </button>
+    <button type="button" @click="removeArticle">
+      <img src="@/assets/remove.png" alt="" />
     </button>
   </div>
 </template>
 
 <script>
 import ArticleCard from "@/components/ArticleCard.vue";
-import axios from "axios";
+import Axios from "@/services/callerService";
 
+const deleteArticleUrl =
+  "http://localhost:3001/restaurant/" +
+  localStorage.getItem("mongoUserId") +
+  "/article/delete";
 export default {
   name: "RestaurantArticleCard",
   props: {
@@ -18,10 +25,23 @@ export default {
   },
   methods: {
     goToUpdatePage() {
-      this.$store.commit('setArticle', this.item);
-      console.log('in store : ', this.$store.getters.getArticle);
-      this.$router.push({ name: 'articleUpdate', params: { id: this.item["id"].slice(-5)}});
-    }
+      this.$store.commit("setArticle", this.item);
+      console.log("in store : ", this.$store.getters.getArticle);
+      this.$router.push({
+        name: "articleUpdate",
+        params: { id: this.item["id"].slice(-5) },
+      });
+    },
+    removeArticle() {
+      console.log('article id: ',this.item["id"])
+      Axios.delete(deleteArticleUrl, {data:{
+        articleId: this.item["id"],
+        userId: localStorage.getItem("userId"),
+      }
+      }).then((response) => {
+        console.log("article deleted");
+      });
+    },
   },
   components: {
     ArticleCard,
@@ -30,5 +50,4 @@ export default {
 </script>
 
 <style>
-
 </style>
