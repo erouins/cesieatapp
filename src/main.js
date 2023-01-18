7612a281fdd126b398016f93d646cad363262988
LilianDubois <liliandubois1209@gmail.com>
2023-01-18 11:13:27 +0100

    Payment session

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+const stripe = Stripe('pk_test_51MEZJPLfKONO35vsu4eZDVilCBbmKWKupxY781MnczHE5WKsgCuyb49CSQRopPw0PZzCmo17DhmxSOs127SqQQ7F00msnpAFKN');
 
-
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+.use(store)
+.use(router)
+.use(stripe)
+.mount('#app')
---
+++ src/views/clients/components/OrderCheck.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+       
+    </div>
+</template>
+
+<script>
+
+import Axios from '@/services/callerService';
+const url = "http://localhost:3001/client/"+localStorage.getItem("mongoUserId")+"/create-checkout-session"
+
+export default {
+    name:'OrderCheck',
+
+    methods:{
+        payOrder(){
+              Axios
+                .post(
+                url,
+                { id: this.orderId}
+                ).then((response) => {
+                if(response.status == 200){
+                    window.open(response.data.url, '_blank', 'noreferrer');
+                }
+                else {
+                    console.log("erreur pay  ")
+                }
+            });
+        }
+    },
+
+
+    mounted(){
+            Axios
+                .post(
+                url,
+                { id: this.orderId}
+                ).then((response) => {
+                if(response.status == 200){
+                    window.open(response.data.url, '_blank', 'noreferrer');
+                }
+                else {
+                    console.log("erreur pay  ")
+                }
+            });
+    
+    },
+}
+
+</script>
